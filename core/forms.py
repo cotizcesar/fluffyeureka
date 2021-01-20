ebbb3dc26013c7f9df80ca9e2bf55a1602232cea
Cesar Cotiz <cesarcotiz@habi.co>
2021-01-19 21:44:25 -0500

    Added: validation

--- core/forms.py
+++ core/forms.py
@@ -11,6 +11,19 @@ from allauth.account.forms import SignupForm
 from .validators import username_validator
 
 
+class SignUpForm(forms.ModelForm):
+    username = forms.CharField(max_length=30, validators=[username_validator])
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name")
+        help_texts = {
+            "username": "Use your IGN",
+            "first_name": "If you want to use your first name you can place it here.",
+            "last_name": "If you use your first name, you should also put your last name, so its easier to know what your name really is.",
+        }
+
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
@@ -28,20 +41,26 @@ class UserProfileForm(forms.ModelForm):
         help_texts = {
             "avatar": "Only images in JPG and PNG are allowed.",
             "biography": "Use this space for text, you have a limit of 160 characters.",
+            "game": "Select the game you like.",
         }
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("text", "image")
+        fields = ("text", "image", "video")
         help_texts = {
             "text": "Use this space for text, you have a limit of 280 characters.",
             "image": "Only images in JPG and PNG are allowed.",
             "video": "Copy and paste an URL from the following sites: YouTube.com, Twitch.tv, Vimeo.com or Giphy.com.",
         }
 
+
 class DodoForm(forms.ModelForm):
     class Meta:
         model = Dodo
-        fields = ("text", "code", "image", )
+        fields = (
+            "text",
+            "code",
+            "image",
+        )
